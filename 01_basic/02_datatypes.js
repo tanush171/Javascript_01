f2dde2baf3dbf5b41caa09fcf4390d17509a7cde
Thanush singh jasrotia <tanush7466@gmail.com>
2026-06-08 10:05:36 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat every js file as a new version.
+// alert(3+3) // we are using node js not browser 
+
+console.log(3+3); console.log("Tanush") // code readability 
+
+let age = 18 
+let name = "Tanush"
+let isLoggedIn = true 
+let state; 
+
+console.log(typeof "Tanush");
+console.log(typeof null); //object
+console.log(typeof undefined);
