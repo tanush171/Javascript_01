6cccc2f244ff09f3d8d64383721e6f3db9e61304
Thanush singh jasrotia <tanush7466@gmail.com>
2026-06-08 16:57:45 +0000

    Conversion of Datatypes

---
+++ 01_basic/03_onversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "33ana" 
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33naa" => NaN (not a number)
+// true => 1; ,false => 0;
+
+/*
+let isLoggedIn = "Tanush"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+*/
+// "" => false 
+// "Tanush" => True 
+// 1 => true , 0 => false 
+
+let somenumber = 33
+let isString = String(somenumber)
+console.log(isString);
+console.log(typeof isString);
