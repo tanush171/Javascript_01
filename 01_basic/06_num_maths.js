3913953e871405f309ade699769042520cc3804d
Thanush singh jasrotia <tanush7466@gmail.com>
2026-06-10 08:35:35 +0000

    math and num

---
+++ 01_basic/06_num_maths.js
@@ -0,0 +1,15 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(5.6));
+console.log(Math.floor(5.3));
+console.log(Math.min(4,5,6,54,3))
+console.log(Math.max(43,4,3,54,22,3));
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random() * (max- min + 1)) + min)
