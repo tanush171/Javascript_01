e23b0071ec6411655dc996c12b33bf9ce592af54
Thanush singh jasrotia <tanush7466@gmail.com>
2026-06-11 10:51:47 +0000

    de structure object

---
+++ 02_basic/05_object.js
@@ -0,0 +1,26 @@
+const course = {
+    coursename: "JS in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+
+}
+// course.courseInstructor 
+
+const {courseInstructor} = course // another syntax to extract value
+const {courseInstructor: instructor} = course 
+
+console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "Tanush",
+//     "coursename": "js in hindi from youtube",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
