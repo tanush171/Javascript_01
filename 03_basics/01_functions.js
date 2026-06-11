41c7d2ff2f7520ffbd6e07a47c9793875af29a65
Thanush singh jasrotia <tanush7466@gmail.com>
2026-06-11 17:15:55 +0000

    Function object & Array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -32,5 +32,41 @@ function loginUserMessage(username = "sam"){
 }
 
 // console.log(loginUserMessage("Tanush"))
-console.log(loginUserMessage()) // undefined
+// console.log(loginUserMessage()) // undefined
+
+function calculateCartPrice(vale1, val2, ...num1) { // ...restoperator/ spread operator
+    return num1
+}
+
+console.log(calculateCartPrice(200, 300, 334, 500))
+// first value go to Val1 , second val2 , rest in ...num1
+
+const user = {
+    username: "Tanush ",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} price is ${anyObject.price}`)
+}
+
+// handleObject(user)
+
+handleObject({
+    username: "sam",
+    price: 1222
+})
+
+
+// PAss the array in fucntion
+
+const myarray = [200, 2000,3000 , 3009, 400]
+
+function returnSecondValue(getarray){
+       return getarray[1]
+}
+
+// console.log(returnSecondValue(myarray));
+
+console.log(returnSecondValue([400,3,445,544,22]));
 
