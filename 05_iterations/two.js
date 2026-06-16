8bfa1543f5b78052bf26e1cddc5bfe132b4a29c3
Thanush singh jasrotia <tanush7466@gmail.com>
2026-06-16 06:32:21 +0000

    While and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+let index = 0
+
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+    
+// }
+
+let Myarray = ["Flash", "batman","superman"]
+
+let arr = 0
+
+while( arr < Myarray.length){
+    // console.log(`my superheros ${Myarray[arr]}`);
+    arr = arr + 1
+}
+
+
+let score = 1
+// let score = 11 print one time 
+
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
